6a47df319134683af7a16baaa4c24b1e27d3c626
adimunawar31 <munawar.adi31@gmail.com>
2023-02-13 07:33:20 +0700

    change location for presence

--- lib/company_data.dart
+++ lib/company_data.dart
@@ -2,15 +2,15 @@ class CompanyData {
   static String defaultPassword = 'password';
   static String defaultRole = 'pegawai';
   static Map<String, dynamic> office = {
-    // 'latitude': -6.8032141,
-    // 'longitude': 108.4839333,
+    'latitude': -6.8032141,
+    'longitude': 108.4839333,
 
     // Bima
     // 'latitude': -6.7295211,
     // 'longitude': 108.5370705,
 
     // Home
-    'latitude': -6.8050124,
-    'longitude': 108.4787524,
+    // 'latitude': -6.8050124,
+    // 'longitude': 108.4787524,
   };
 }
